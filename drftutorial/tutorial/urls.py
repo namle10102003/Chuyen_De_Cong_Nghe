a1e92af962f39495310eb46dbf6c0f4ff186b612
namle10102003 <Namle10102003@gmail.com>
2025-09-16 20:04:26 +0700

    Done Authentication and Permissions Exercise

--- drftutorial/tutorial/urls.py
+++ drftutorial/tutorial/urls.py
@@ -26,6 +26,10 @@ router.register(r'groups', views.GroupViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('', include('snippets.urls')),
+]
+
+# Thêm login/logout cho browsable API
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls')),
 ]
